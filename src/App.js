f420094d440b47adf02438ccd58ed49e6df74323
romson <romerolijkwan@gmail.com>
2019-05-06 20:11:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Form from './components/form';
+import Recipes from './components/recipes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = '4499060ddbbeffade1c1fb4c7a7e683c';
+
+
+
+class App extends Component {
+  state = {
+    recipes: []
+  }
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(`http://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=5`);
+
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
+      </div>
+    );
+  }
 }
 
 export default App;
